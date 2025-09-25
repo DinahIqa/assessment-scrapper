c8ab0c9521c67c232d27356fec75394e59a2a3c4
dinahiqa <dinahiqa@umich.edu>
2025-09-25 13:53:44 +0300

    need to figure to include symbols in title

--- redditscrapper.py
+++ redditscrapper.py
@@ -1,25 +1,6 @@
 import requests
 import json
 import time
-from bs4 import BeautifulSoup
-
-#def getsoup(url):
-#    response = requests.get(url, headers)
-#    return response.text
-
-# url = "https://www.reddit.com/r/uofm/"
-# headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'}
-# page = requests.get(url, headers=headers)
-# soup = BeautifulSoup(page.text, 'html.parser')
-
-# print(soup)
-
-# domains = soup.find_all("span", class_="domain")
-# for domain in domains:
-#     if domain != "(self.uofm)":
-#         continue
-
-#     print(domain.text)
 
 ##try json
 base_url = "https://www.reddit.com/r/uofm/.json"
@@ -29,18 +10,18 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 image_post = []
 after = None
 
-for i in range(2):
+for i in range(10):
     url = base_url if not after else f"{base_url}?after={after}"
-    print("Fetching: ", url)
+    print("Fetching page", i+1)
     page = requests.get(url, headers=headers)
     json_data = page.json()
 
-    for post in json_data["data"]["children"]:
+    for post in json_data["data"]["children"]: #go thru every post in page
         post_data = post["data"]
         post_title = post_data["title"]
-        image_url = post_data.get("url_overridden_by_dest")
+        image_url = post_data.get("url_overridden_by_dest") #images are usually linked here
 
-        if image_url:
+        if image_url and image_url.endswith((".jpeg", ".png")): #ensure there is image in the post
             image_post.append({
                 "post_title": post_title,
                 "image_url": image_url
